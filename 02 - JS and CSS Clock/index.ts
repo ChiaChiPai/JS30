157d20cc66213781456bbb9ead189d0c554738ec
White Pai <white_pai@appdet.com>
2022-02-10 16:01:11 +0800

    fix: 🐛 修正 transition 動畫從 59-0 的跳動

--- 02 - JS and CSS Clock/index.ts
+++ 02 - JS and CSS Clock/index.ts
@@ -7,6 +7,16 @@ const getCurrentTime = () => {
   const hour = now.getHours() > 12? now.getHours() - 12 :now.getHours()
   const min = now.getMinutes()
   const sec = now.getSeconds()
+  if (min === 0) {
+    minElement.style.transition = 'all 0s'
+  } else {
+    minElement.style.transition = 'all 0.05s'
+  }
+  if (sec === 0) {
+    secondElement.style.transition = 'all 0s'
+  } else {
+    secondElement.style.transition = 'all 0.05s'
+  }
   hourElement.style.transform = `rotate(${30 * hour+ (min / 12 / 60) * 360}deg)`
   minElement.style.transform = `rotate(${6 * min}deg)`
   secondElement.style.transform = `rotate(${6 * sec}deg)`
