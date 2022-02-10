8773b68133e38b4486c90105fa3e4f0fa3da10f2
White Pai <white_pai@appdet.com>
2022-02-10 15:29:21 +0800

    feat: 🎸 02 function

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('ge');
+const hourElement = document.querySelector("#hour");
+const minElement = document.querySelector("#min");
+const secondElement = document.querySelector("#second");
+setInterval(() => {
+    const now = new Date();
+    const hour = now.getHours() > 12 ? now.getHours() - 12 : now.getHours();
+    const min = now.getMinutes();
+    const sec = now.getSeconds();
+    hourElement.style.transform = `rotate(${30 * hour}deg)`;
+    minElement.style.transform = `rotate(${6 * min}deg)`;
+    secondElement.style.transform = `rotate(${6 * sec}deg)`;
+}, 1000);
--- 02 - JS and CSS Clock/index.ts
+++ 02 - JS and CSS Clock/index.ts
@@ -1,1 +1,13 @@
-console.log('ge')
+const hourElement = document.querySelector("#hour") as HTMLElement
+const minElement = document.querySelector("#min") as HTMLElement
+const secondElement = document.querySelector("#second") as HTMLElement
+
+setInterval(() => {
+  const now = new Date()
+  const hour = now.getHours() > 12? now.getHours() - 12 :now.getHours()
+  const min = now.getMinutes()
+  const sec = now.getSeconds()
+  hourElement.style.transform = `rotate(${30 * hour}deg)`
+  minElement.style.transform = `rotate(${6 * min}deg)`
+  secondElement.style.transform = `rotate(${6 * sec}deg)`
+}, 1000)
